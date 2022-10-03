71189ed0d352d22387fafd7847ce6aabee1d843f
Miguel Ruiz <miguelruiz@clearchannel.com>
2022-10-03 11:18:19 -0700

    docker container with gcp emulators starts now

--- local-pubsub.sh
+++ local-pubsub.sh
@@ -23,16 +23,17 @@ while true; do
 	fi
 done
 
-read -p "Enter project name:" project_name
-read -p "Enter topic name:" topic_name
-read -p "Enter subscription name:" subscription_name
+#read -p "Enter project name:" project_name
+#read -p "Enter topic name:" topic_name
+#read -p "Enter subscription name:" subscription_name
 
 port_array=(${ports//:/ })
+host_port="${port_array[1]}"
 
-docker run --rm -ti -p $ports \
+docker run -d --rm -ti -p $ports \
     gcr.io/google.com/cloudsdktool/cloud-sdk:$version-emulators \
     gcloud beta emulators pubsub start \
       --project=abc \
-      --host-port=0.0.0.0:$port_array[1]
+      --host-port=0.0.0.0:$host_port
 
 echo "end"
