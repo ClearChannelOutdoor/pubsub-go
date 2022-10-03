27554b3eae851f285379a1e36e738d7ad4ac7f81
Miguel Ruiz <miguelruiz@clearchannel.com>
2022-10-03 10:52:16 -0700

    added step for version, and link to gcp repo to see which versions are available

--- local-pubsub.sh
+++ local-pubsub.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 kPORT_REGEX="[0-9]:[0-9]"
+kGCP_REGISTRY_URL="https://console.cloud.google.com/gcr/images/google.com:cloudsdktool/GLOBAL/cloud-sdk?gcrImageListsize=30"
 
 while true; do
 	read -p "Enter docker port mapping (\"3080:3085\" is default):" ports
 	if [[ $ports =~ $kPORT_REGEX ]]; then break; fi
 	echo "Port mapping please..."
+done
+
+while true; do
+	read -p "Enter docker image version, or type browse to see a list of available versions:" version
+	if [[ $version -ne "browse" ]]; then break; fi
+	if [[ $version -eq "browse" ]]; then
+		if which xdg-open >/dev/null; then
+			xdg-open "$kGCP_REGISTRY_URL"
+		elif which open >/dev/null; then
+			open "$kGCP_REGISTRY_URL"
+		else
+			echo "nothing to open with"
+		fi
+	fi
 done
 
 read -p "Enter project name:" project_name
@@ -15,4 +30,11 @@ read -p "Enter subscription name:" subscription_name
 port_array=(${ports//:/ })
 echo ${port_array[0]}
 echo ${port_array[1]}
+
+#docker run --rm -ti -p $ports \
+#    gcr.io/google.com/cloudsdktool/cloud-sdk:$version-emulators \
+#    gcloud beta emulators pubsub start \
+#      --project=abc \
+#      --host-port=0.0.0.0:8085
+
 echo "end"
