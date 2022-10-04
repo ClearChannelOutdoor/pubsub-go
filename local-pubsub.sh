d104f47192d5cd84a0a57be8409a883a52d5d8cc
Miguel Ruiz <miguelruiz@clearchannel.com>
2022-10-04 14:07:28 -0700

    beautify script

--- local-pubsub.sh
+++ local-pubsub.sh
@@ -25,44 +25,44 @@ kWAIT_TIME=20
 kADD_MESSAGE_URL="/v1/projects/\$PROJECT_ID/topics/\$TOPIC_ID:publish"
 
 while [[ $# -gt 0 ]]; do
-	case $1 in
-		-m|--generate-message)
-			echo -e "${BROWN}NOTE:${NC} this option can only generate json to create one message at a time."
-			read -p "$(echo -e "Enter ${CYAN}message data${NC} or ${CYAN}file path${NC}: ")" message_data
-			if [[ -f "$message_data" ]]; then
-				encoded_string=$(openssl base64 -in $message_data | tr -d '\n')
-			else
-				encoded_string=$(echo $message_data | openssl base64)
-			fi
-			echo -e "{\n\t${LIGHT_GREEN}\"messages\"${NC}:[\n\t\t{\n\t\t\t${LIGHT_GREEN}\"data\"${NC}:\"$encoded_string\"\n\t\t}\n\t]\n}\n"
-			echo -e "To publish message, ${RED}POST${NC} this response body to ${LIGHT_BLUE}$PUBSUB_EMULATOR_HOST$kADD_MESSAGE_URL${NC}"
-			exit
-			;;
-		*)
-			break
-			;;
-	esac
+  case $1 in
+    -m | --generate-message)
+      echo -e "${BROWN}NOTE:${NC} this option can only generate json to create one message at a time."
+      read -p "$(echo -e "Enter ${CYAN}message data${NC} or ${CYAN}file path${NC}: ")" message_data
+      if [[ -f "$message_data" ]]; then
+        encoded_string=$(openssl base64 -in $message_data | tr -d '\n')
+      else
+        encoded_string=$(echo $message_data | openssl base64)
+      fi
+      echo -e "{\n\t${LIGHT_GREEN}\"messages\"${NC}:[\n\t\t{\n\t\t\t${LIGHT_GREEN}\"data\"${NC}:\"$encoded_string\"\n\t\t}\n\t]\n}\n"
+      echo -e "To publish message, ${RED}POST${NC} this response body to ${LIGHT_BLUE}$PUBSUB_EMULATOR_HOST$kADD_MESSAGE_URL${NC}"
+      exit
+      ;;
+    *)
+      break
+      ;;
+  esac
 
 done
 
 while true; do
-	read -p "Enter docker port mapping: " ports
-	if [[ $ports =~ $kPORT_REGEX ]]; then break; fi
-	echo "Port mapping please...\n"
+  read -p "Enter docker port mapping: " ports
+  if [[ $ports =~ $kPORT_REGEX ]]; then break; fi
+  echo "Port mapping please...\n"
 done
 
 while true; do
-	read -p "$(echo -e "\nEnter docker image version, or ${RED}type browse${NC} to see a list of available versions:")" version
-	if [[ "$version" != "browse" ]]; then break; fi
-	if [[ "$version" == "browse" ]]; then
-		if which xdg-open >/dev/null; then
-			xdg-open "$kGCP_REGISTRY_URL"
-		elif which open >/dev/null; then
-			open "$kGCP_REGISTRY_URL"
-		else
-			echo "nothing to open with"
-		fi
-	fi
+  read -p "$(echo -e "\nEnter docker image version, or ${RED}type browse${NC} to see a list of available versions:")" version
+  if [[ "$version" != "browse" ]]; then break; fi
+  if [[ "$version" == "browse" ]]; then
+    if which xdg-open > /dev/null; then
+      xdg-open "$kGCP_REGISTRY_URL"
+    elif which open > /dev/null; then
+      open "$kGCP_REGISTRY_URL"
+    else
+      echo "nothing to open with"
+    fi
+  fi
 done
 
 read -p "$(echo -e "\nEnter ${CYAN}Project Name${NC}: ")" project_name
@@ -74,10 +74,10 @@ localhost_port=${port_array[0]}
 host_port="${port_array[1]}"
 
 docker run -d --rm -ti -p $ports \
-    gcr.io/google.com/cloudsdktool/cloud-sdk:$version-emulators \
-    gcloud beta emulators pubsub start \
-      --project=abc \
-      --host-port=0.0.0.0:$host_port
+  gcr.io/google.com/cloudsdktool/cloud-sdk:$version-emulators \
+  gcloud beta emulators pubsub start \
+  --project=abc \
+  --host-port=0.0.0.0:$host_port
 
 echo -e "\nWaiting ${RED}$kWAIT_TIME${NC} seconds for container to start"
 sleep $kWAIT_TIME
@@ -88,8 +88,8 @@ echo -e "${GREEN}Completed Topic Creation${NC}"
 
 echo -e "\n${CYAN}Creating Subscription${NC}"
 curl -X PUT -H "Content-Type:application/json" \
-	--data "{\"topic\":\"projects/$project_name/topics/$topic_name\"}"\
-	"http://localhost:$localhost_port/v1/projects/$project_name/subscriptions/$subscription_name"
+  --data "{\"topic\":\"projects/$project_name/topics/$topic_name\"}" \
+  "http://localhost:$localhost_port/v1/projects/$project_name/subscriptions/$subscription_name"
 
 echo -e "${GREEN}Completd Subscription Creation${NC}"
 
